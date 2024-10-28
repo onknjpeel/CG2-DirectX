5fccf420f9e3277ee02ef1f7bf1e16d23fe49ebd
onknjpeel <k023g0103@g.neec.ac.jp>
2024-10-28 15:30:17 +0900

    <cstdint>の位置を修正

--- WinApp.cpp
+++ WinApp.cpp
@@ -1,6 +1,5 @@
 #include "WinApp.h"
 #include "externals/imgui/imgui.h"
-#include <cstdint>
 
 extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
 
