0ab4dd1377a79965402b712eabc1929d0adf1af2
onknjpeel <k023g0103@g.neec.ac.jp>
2024-11-18 14:09:02 +0900

    libの消し忘れを修正

--- main.cpp
+++ main.cpp
@@ -24,10 +24,7 @@ extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hwnd, UINT msg
 
 #pragma comment(lib,"d3d12.lib")
 #pragma comment(lib,"dxgi.lib")
-#pragma comment(lib,"dxguid.lib")
 #pragma comment(lib,"dxcompiler.lib")
-#pragma comment(lib,"dinput8.lib")
-#pragma comment(lib,"dxguid.lib")
 
 #pragma region 構造体群
 struct Vector2 {
